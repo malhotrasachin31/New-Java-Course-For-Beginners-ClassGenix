d9705e2c8757a7732930326b482582a4546da4ca
malhotrasachin31 <72509220+malhotrasachin31@users.noreply.github.com>
2021-12-17 00:59:35 +0530

    Add files via upload

---
+++ JavaLiterals.java
@@ -0,0 +1,48 @@
+
+// video number -4 ie java literals
+
+package javaliterals;
+
+public class JavaLiterals {
+
+    public static void main(String[] args) {
+       
+        // age = variable name
+        
+        // byte literal = 22
+        // range of byte = -128 to +127
+        byte age = 12;
+        
+        // short literal can take upto 4 digits
+        short value = 3490;
+        
+        // long can take upto 9 digit without L 
+        long value2 = 6789456789090909009L;
+        
+        // integer literal can take input upto 9 digit
+        int value3 =  999999999;
+        
+        // character literal
+        char name = 'A';
+        
+        //boolean literal default value is false
+        boolean result = false;
+        
+        // float literal
+        float kilometer = 23.2f;
+        
+        // double literal
+        double kilometers = 34.788d;
+        
+        System.out.println("the byte literal is "+age);
+        System.out.println("the short literal is "+value);
+        System.out.println("the integer literal is "+value3);
+        System.out.println("the long literal is "+value2);
+        System.out.println("the boolean literal is "+result);
+        System.out.println("the char literal is "+name);
+        System.out.println("the float literal is "+kilometer); 
+        System.out.println("the double literal is "+kilometers); 
+        
+    }
+    
+}
