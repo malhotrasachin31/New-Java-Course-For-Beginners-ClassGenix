e172907143a23d6e7b46a33eaf24c77d7029e59f
malhotrasachin31 <72509220+malhotrasachin31@users.noreply.github.com>
2021-12-17 00:58:45 +0530

    Add files via upload

---
+++ Practice1.java
@@ -0,0 +1,15 @@
+package practice1;
+// this is my first practice question from java data types
+public class Practice1 {
+
+    public static void main(String[] args) {
+        // sum of two numbers
+        
+        int number1 = 2;
+        int number2 = 7;
+        int sum=number1+number2;
+        
+        System.out.println("The sum of "+number1+" and "+number2+" are : "+sum);
+    }
+    
+}
