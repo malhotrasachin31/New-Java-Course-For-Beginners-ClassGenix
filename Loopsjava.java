4a1120a792fc3f80c68076aee597d8dcf32b000e
malhotrasachin31 <72509220+malhotrasachin31@users.noreply.github.com>
2022-04-09 10:02:44 +0530

    Add files via upload

---
+++ Loopsjava.java
@@ -0,0 +1,46 @@
+
+package loopsjava;
+
+public class Loopsjava {
+
+    public static void main(String[] args) {
+       
+       System.out.println("Without Loops...");
+        
+       System.out.println("Sachin");
+       System.out.println("Sachin");
+       System.out.println("Sachin");
+       System.out.println("Sachin");
+       System.out.println("Sachin");
+       
+       System.out.println("With Loops...");
+       
+       // take an example of racing track
+       // laps we need to finish = 5
+       // start = 0
+       
+       int lap = 1;
+       while(lap<=6){
+           System.out.println("Lap number:"+lap);
+           lap++;
+           
+           /*
+            lap = 1<6 = true
+            lap = 2<6 = true
+            lap = 3<6 = true
+            lap = 4<6 = true
+            lap = 5<6 = true
+            lap = 6<6 = true
+           
+           */
+           
+       }
+       
+       
+       
+       
+       
+       
+    }
+    
+}
