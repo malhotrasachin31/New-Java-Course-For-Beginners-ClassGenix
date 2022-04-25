b354fd587612345d331d3ad9399f6b1e0592b8a9
malhotrasachin31 <72509220+malhotrasachin31@users.noreply.github.com>
2022-04-25 20:26:11 +0530

    Add files via upload

---
+++ dowhileloops.java
@@ -0,0 +1,25 @@
+
+package loopsjava;
+public class dowhileloops {
+    public static void main(String args[]){
+        
+        System.out.println("All about while loops");
+        
+        int lap = 100;
+        while(lap<10){// condition==false
+            System.out.println("Keep racing..");
+            lap++;
+        }
+        
+        System.out.println("All about do while loops");
+        
+        do{
+          System.out.println("Keep racing..");
+          lap++;
+        }while(lap<10);
+                
+        
+        
+        
+    }
+}
