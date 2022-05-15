3d3ce73b8942c1cae7da134aa70f2f8c45ba2372
malhotrasachin31 <72509220+malhotrasachin31@users.noreply.github.com>
2022-05-15 16:31:52 +0530

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,53 @@
+package pkgfor.loops;
+
+public class ForLoops {
+
+    public static void main(String[] args) {
+        // incrementing for loop
+        int n=50;
+        /*for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+        */
+        // decrementing for loop in java
+        
+        /*for(int i=n;i>0;i--){
+            System.out.println(i);
+        }*/
+        
+        
+        /*
+        for(initialize;boolean check;updation){
+        
+        }
+        */
+        
+        /*
+        i=1;
+        1<=5 = true print 1
+        i=2 
+        2<=5 = true print 2
+        */
+        
+        // Quick quizz - 1.print n natural numbers
+        // 2. print n even numbers
+        // 3. print n odd numbers
+        
+        // 4. do all above question with the help of while and do while loops
+        
+        /*
+        even number = n*2 (0*2=0 , 1*2=2 , 2*2=4 , 3*2=6)
+        odd number = n*2+1 (0*2+1=1 , 1*2+1=3 , 2*2+1=5 , 3*2+1=7)
+        */
+        
+        int num = 1000;
+        for(int j=1;j<=num;j++){
+            System.out.println(j*2+1);
+        }
+        
+        
+        
+        
+    }
+    
+}
